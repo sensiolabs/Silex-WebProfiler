621249a28c3291c9a862759904f1e280db7fc36e
Ozberk Ercin <ozberk.ercin@researchgate.net>
2016-12-24 17:09:01 +0100

    apply coding standard patch

--- WebProfilerServiceProvider.php
+++ WebProfilerServiceProvider.php
@@ -103,22 +103,42 @@ class WebProfilerServiceProvider implements ServiceProviderInterface, Controller
 
         $app['data_collectors'] = function ($app) {
             return array(
-                'config' => function ($app) { return new ConfigDataCollector('Silex', Application::VERSION); },
-                'request' => function ($app) { return new RequestDataCollector(); },
-                'exception' => function ($app) { return new ExceptionDataCollector(); },
-                'events' => function ($app) { return new EventDataCollector($app['dispatcher']); },
-                'logger' => function ($app) { return new LoggerDataCollector($app['logger']); },
-                'time' => function ($app) { return new TimeDataCollector(null, $app['stopwatch']); },
-                'router' => function ($app) { return new RouterDataCollector(); },
-                'memory' => function ($app) { return new MemoryDataCollector(); },
+                'config' => function ($app) {
+                    return new ConfigDataCollector('Silex', Application::VERSION);
+                },
+                'request' => function ($app) {
+                    return new RequestDataCollector();
+                },
+                'exception' => function ($app) {
+                    return new ExceptionDataCollector();
+                },
+                'events' => function ($app) {
+                    return new EventDataCollector($app['dispatcher']);
+                },
+                'logger' => function ($app) {
+                    return new LoggerDataCollector($app['logger']);
+                },
+                'time' => function ($app) {
+                    return new TimeDataCollector(null, $app['stopwatch']);
+                },
+                'router' => function ($app) {
+                    return new RouterDataCollector();
+                },
+                'memory' => function ($app) {
+                    return new MemoryDataCollector();
+                },
             );
         };
 
         if (isset($app['form.resolved_type_factory']) && class_exists('\Symfony\Component\Form\Extension\DataCollector\FormDataCollector')) {
-            $app['data_collectors.form.extractor'] = function () { return new FormDataExtractor(); };
+            $app['data_collectors.form.extractor'] = function () {
+                return new FormDataExtractor();
+            };
 
             $app->extend('data_collectors', function ($collectors, $app) {
-                $collectors['form'] = function ($app) { return new FormDataCollector($app['data_collectors.form.extractor']); };
+                $collectors['form'] = function ($app) {
+                    return new FormDataCollector($app['data_collectors.form.extractor']);
+                };
 
                 return $collectors;
             });
